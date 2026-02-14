fc07d2517c8235201589f0302db51c63fe3b8bef
fcadena <fcadena@corcentric.com>
2026-02-13 20:38:45 -0500

    feat: fixed verel configuration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'tsconfig-paths/register';
 import 'dotenv/config';
 
 import {
@@ -9,16 +10,6 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
-
-// Register path aliases only in development
-// In production (webpack), paths are resolved by webpack alias
-
-void (
-  process.env.NODE_ENV !== 'production' &&
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  require('tsconfig-paths/register')
-);
-
 import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
 import { AllConfigType } from './config/config.type';
